18697fded0ec786411624dad6d9faa416ce9245d
Henry Jorgensen <jorhg002@mymail.unisa.edu.au>
2022-05-18 13:59:17 +0930

    added some attributes and constructor to item class

--- WorkshopImplementation/Item.h
+++ WorkshopImplementation/Item.h
@@ -1,5 +1,21 @@
 #pragma once
+#include <string>
+
+using namespace std;
 class Item
 {
+public:
+	Item(string title, string registration, int year);
+	~Item();
+
+	string name;
+	string registration;
+	string state;
+	int year;
+
+private:
+	
+	
+
 };
 
--- WorkshopImplementation/WorkshopImplementation.cpp
+++ WorkshopImplementation/WorkshopImplementation.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <string>
 
 int main()
 {
